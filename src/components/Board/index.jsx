0c67b1e385d8f7020a00b943151d24b62c81a924
sushant-menon <sushantnambiar@gmail.com>
2023-12-06 23:44:18 +0530

    started working with the canvas part and made some event listeners

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -11,6 +11,23 @@ const Board = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
+    const changeConfig = () => {
+      context.strokeStyle = color;
+      context.lineWidth = size;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    changeConfig();
+  }, [color, size]);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   }, []);
